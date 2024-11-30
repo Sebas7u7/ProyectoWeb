632af361f22dcedfeea470dfd9ca94fe1058d1d0
Sebas <=sebastianmahechat@gmail.com>
2024-11-30 16:14:11 -0500

    controladores

---
+++ ProyectoWeb/Controllers/NegocioController.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProyectoWeb.Models;
+
+namespace ProyectoWeb.Controllers
+{
+    public class NegocioController : Controller
+    {
+        private readonly DbventasContext _context;
+
+        public NegocioController(DbventasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Negocio
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Negocios.ToListAsync());
+        }
+
+        // GET: Negocio/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var negocio = await _context.Negocios
+                .FirstOrDefaultAsync(m => m.IdNegocio == id);
+            if (negocio == null)
+            {
+                return NotFound();
+            }
+
+            return View(negocio);
+        }
+
+        // GET: Negocio/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Negocio/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdNegocio,UrlLogo,NombreLogo,NumeroDocumento,Nombre,Correo,Direccion,Telefono,PorcentajeImpuesto,SimboloMoneda")] Negocio negocio)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(negocio);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(negocio);
+        }
+
+        // GET: Negocio/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var negocio = await _context.Negocios.FindAsync(id);
+            if (negocio == null)
+            {
+                return NotFound();
+            }
+            return View(negocio);
+        }
+
+        // POST: Negocio/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdNegocio,UrlLogo,NombreLogo,NumeroDocumento,Nombre,Correo,Direccion,Telefono,PorcentajeImpuesto,SimboloMoneda")] Negocio negocio)
+        {
+            if (id != negocio.IdNegocio)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(negocio);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!NegocioExists(negocio.IdNegocio))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(negocio);
+        }
+
+        // GET: Negocio/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var negocio = await _context.Negocios
+                .FirstOrDefaultAsync(m => m.IdNegocio == id);
+            if (negocio == null)
+            {
+                return NotFound();
+            }
+
+            return View(negocio);
+        }
+
+        // POST: Negocio/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var negocio = await _context.Negocios.FindAsync(id);
+            if (negocio != null)
+            {
+                _context.Negocios.Remove(negocio);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool NegocioExists(int id)
+        {
+            return _context.Negocios.Any(e => e.IdNegocio == id);
+        }
+    }
+}
---
+++ ProyectoWeb/Controllers/VentaController.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ProyectoWeb.Models;
+
+namespace ProyectoWeb.Controllers
+{
+    public class VentaController : Controller
+    {
+        private readonly DbventasContext _context;
+
+        public VentaController(DbventasContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Venta
+        public async Task<IActionResult> Index()
+        {
+            var dbventasContext = _context.Venta.Include(v => v.IdTipoDocumentoVentaNavigation).Include(v => v.IdUsuarioNavigation);
+            return View(await dbventasContext.ToListAsync());
+        }
+
+        // GET: Venta/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ventum = await _context.Venta
+                .Include(v => v.IdTipoDocumentoVentaNavigation)
+                .Include(v => v.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdVenta == id);
+            if (ventum == null)
+            {
+                return NotFound();
+            }
+
+            return View(ventum);
+        }
+
+        // GET: Venta/Create
+        public IActionResult Create()
+        {
+            ViewData["IdTipoDocumentoVenta"] = new SelectList(_context.TipoDocumentoVenta, "IdTipoDocumentoVenta", "IdTipoDocumentoVenta");
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario");
+            return View();
+        }
+
+        // POST: Venta/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("IdVenta,NumeroVenta,IdTipoDocumentoVenta,IdUsuario,DocumentoCliente,NombreCliente,SubTotal,ImpuestoTotal,Total,FechaRegistro")] Ventum ventum)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(ventum);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdTipoDocumentoVenta"] = new SelectList(_context.TipoDocumentoVenta, "IdTipoDocumentoVenta", "IdTipoDocumentoVenta", ventum.IdTipoDocumentoVenta);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", ventum.IdUsuario);
+            return View(ventum);
+        }
+
+        // GET: Venta/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ventum = await _context.Venta.FindAsync(id);
+            if (ventum == null)
+            {
+                return NotFound();
+            }
+            ViewData["IdTipoDocumentoVenta"] = new SelectList(_context.TipoDocumentoVenta, "IdTipoDocumentoVenta", "IdTipoDocumentoVenta", ventum.IdTipoDocumentoVenta);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", ventum.IdUsuario);
+            return View(ventum);
+        }
+
+        // POST: Venta/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("IdVenta,NumeroVenta,IdTipoDocumentoVenta,IdUsuario,DocumentoCliente,NombreCliente,SubTotal,ImpuestoTotal,Total,FechaRegistro")] Ventum ventum)
+        {
+            if (id != ventum.IdVenta)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(ventum);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!VentumExists(ventum.IdVenta))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["IdTipoDocumentoVenta"] = new SelectList(_context.TipoDocumentoVenta, "IdTipoDocumentoVenta", "IdTipoDocumentoVenta", ventum.IdTipoDocumentoVenta);
+            ViewData["IdUsuario"] = new SelectList(_context.Usuarios, "IdUsuario", "IdUsuario", ventum.IdUsuario);
+            return View(ventum);
+        }
+
+        // GET: Venta/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var ventum = await _context.Venta
+                .Include(v => v.IdTipoDocumentoVentaNavigation)
+                .Include(v => v.IdUsuarioNavigation)
+                .FirstOrDefaultAsync(m => m.IdVenta == id);
+            if (ventum == null)
+            {
+                return NotFound();
+            }
+
+            return View(ventum);
+        }
+
+        // POST: Venta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var ventum = await _context.Venta.FindAsync(id);
+            if (ventum != null)
+            {
+                _context.Venta.Remove(ventum);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool VentumExists(int id)
+        {
+            return _context.Venta.Any(e => e.IdVenta == id);
+        }
+    }
+}
